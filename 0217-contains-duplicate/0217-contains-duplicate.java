db1c4221de53a36221ab2645d9fa62e61eece4fc
Surekha A <surekhamns2106@gmail.com>
2026-08-25 06:40:24 +0530

    Time: 24 ms (28.78%), Space: 93 MB (78.58%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+      Set<Integer>set=new HashSet<>();
+      for(int n:nums){
+        set.add(n);
+      }
+      if(nums.length!=set.size()){
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
+}
