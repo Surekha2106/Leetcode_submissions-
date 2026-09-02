0f082ca3c9883f90bc560737f5ebe7dbc3944b61
Surekha A <surekhamns2106@gmail.com>
2026-09-02 21:57:16 +0530

    Time: 8 ms (61.03%), Space: 43.3 MB (26.95%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String countAndSay(int n) {
+    String res="1";
+    for(int i=1;i<n;i++){
+        int count=1;
+        StringBuilder sb= new StringBuilder();
+        for(int j=1;j<res.length();j++){
+            if(res.charAt(j)== res.charAt(j-1)){
+                count++;
+            }
+            else{
+                sb.append(count);
+                sb.append(res.charAt(j-1));
+                count=1;
+            }
+        }
+        sb.append(count);
+        sb.append(res.charAt(res.length()-1));
+
+        res=sb.toString();
+    } 
+    return res;
+    }
+}
